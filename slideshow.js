0f6a987bef0e40e02fde792a4df1679f2922c004
Anna Laffoon <ree11194@gmail.com>
2022-03-12 21:18:43 -0500

    add kvp to slideshowjs file

---
+++ slideshow.js
@@ -0,0 +1,31 @@
+
+// <img class="slideshow" src="https://cdn.pixabay.com/photo/2020/10/08/00/33/orc-5636526_1280.png"
+// alt="Orc">
+// <img class="slideshow" src="https://cdn.pixabay.com/photo/2021/09/03/22/23/elf-6596691_1280.png"
+// alt="Halfling">
+// <img class="slideshow"
+// src="https://www.maxpixel.net/static/photo/1x/People-Fantasy-Face-Male-Man-Guy-Elf-Model-5389055.png"
+// alt="Elf">
+// <img class="slideshow"
+// src="https://www.maxpixel.net/static/photo/1x/Fighter-Soldier-Man-Sword-Warrior-Armor-Fantasy-5636538.png"
+// alt="Human">
+
+const racesSlideshowImages = [
+    {
+        src: "https://cdn.pixabay.com/photo/2020/10/08/00/33/orc-5636526_1280.png",
+        alt: "Orc",
+    },
+    {
+        src: "https://cdn.pixabay.com/photo/2021/09/03/22/23/elf-6596691_1280.png",
+        alt: "Halfling",
+    },
+    {
+        src: "https://www.maxpixel.net/static/photo/1x/People-Fantasy-Face-Male-Man-Guy-Elf-Model-5389055.png",
+        alt: "Elf",
+    },
+    {
+        src: "https://www.maxpixel.net/static/photo/1x/Fighter-Soldier-Man-Sword-Warrior-Armor-Fantasy-5636538.png",
+        alt: "Human",
+    },
+];
+
